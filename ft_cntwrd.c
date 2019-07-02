d9e2c47979b4b127bc0e90162fe116a5247b5816
Nduduzo HLATSHWAYO <nhlatshw@c4r11s11.wethinkcode.co.za>
2019-07-02 17:42:17 +0200

    saved file

--- ft_cntwrd.c
+++ ft_cntwrd.c
@@ -6,13 +6,13 @@
 /*   By: nhlatshw <nhlatshw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 17:33:06 by nhlatshw          #+#    #+#             */
-/*   Updated: 2019/07/02 17:33:53 by nhlatshw         ###   ########.fr       */
+/*   Updated: 2019/07/02 17:42:01 by nhlatshw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_cntwrd(char const *s, char c)
+int		ft_cntwrd(char const *s, char c)
 {
 	unsigned int	i;
 	int				cntr;
